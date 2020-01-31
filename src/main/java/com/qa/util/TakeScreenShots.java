e0c2963b877c359a49baab23c81048961ad9399a
rabin2004 <rabin2004@User>
2020-01-31 11:32:16 -0500

    updated

--- src/main/java/com/qa/util/TakeScreenShots.java
+++ src/main/java/com/qa/util/TakeScreenShots.java
@@ -2,28 +2,16 @@ package com.qa.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.qa.base.TestBase;
 
 public class TakeScreenShots extends TestBase {
 
 	public static void main(String[] args) throws IOException {
-		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(TestUtil.page_load_timeout, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(TestUtil.implicit_wait, TimeUnit.SECONDS);
-		
-		driver.get(prop.getProperty("url"));
 		
 		// Take sreenshot(.getScreenshotAs method) and store as a file format
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
